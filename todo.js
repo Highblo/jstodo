75b21183b34d538e12be07d5c83d7d0f5bd43263
siotan <nyaa.-525@outlook.jp>
2023-08-02 05:10:14 +0900

    ?

--- todo.js
+++ todo.js
@@ -1,3 +1,4 @@
+// hennnatodo
 const input = document.getElementById('input');
 const form = document.getElementById('form');
 const ul = document.getElementById('ul');
@@ -15,5 +16,20 @@ const saveData = () => {
     todos.push(todo);
     localStorage.setItem('todos', JSON.stringify(todos));
     input.value = '';
+    rendTodo();
   }
+}
+
+const rendTodo = () => {
+  const datas = JSON.parse(localStorage.getItem('todos'));
+  const li = document.createElement('li');
+  li.innerText = datas[datas.length - 1];
+  ul.appendChild(li);
+
+  li.addEventListener('click', (e) => {
+    const removeTodo = todos.findIndex(todo => todo === e.target.innerText);
+    todos.splice(removeTodo, 1);
+    console.log(todos);
+    ul.removeChild(e.target);
+  });
 }
